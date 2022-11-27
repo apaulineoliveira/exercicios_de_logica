6e57399df213a147090059d1bde0326d29500f59
apaulineoliveira <apaulineoliveira@gmail.com>
2022-11-26 23:55:23 -0300

    Update

---
+++ Efetuar operações matemáticas/index.js
@@ -0,0 +1,6 @@
+function calculate(){
+    var a = document.querySelector('value1').value;
+    var b = document.querySelector('value2').value;
+    var select = document.querySelector('input[name="operation"]:checked').value;
+    console.log(select);
+}
