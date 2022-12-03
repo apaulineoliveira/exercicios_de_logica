a9550c7bc08b8fb70aaf21486965c76c61300c76
apaulineoliveira <apaulineoliveira@gmail.com>
2022-12-02 23:23:11 -0300

    Initial commit

---
+++ Var_let_const/index.js
@@ -0,0 +1,14 @@
+//const s = [5, 7, 2];
+function editInPlace() {
+}
+//editInPlace();
+
+//Solução abaixo//
+
+const s = [5, 7, 2];
+function editInPlace() {
+  s[0] = 2;
+  s[1] = 5;
+  s[2] = 7;
+}
+editInPlace();
